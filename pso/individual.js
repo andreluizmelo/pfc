f2dd32d0e05d018280bd1eb80677df66d06cab08
Andre Luizm <andreluiz.melo@outlook.com>
2017-04-11 08:51:28 -0300

    estrutura pso

---
+++ pso/individual.js
@@ -0,0 +1,22 @@
+var _ = require('lodash');
+
+function Individual(genome, fitnessFunction, walkFunction){
+    this.genome = genome;
+    this.walkFunction = walkFunction;
+    this.fitnessFunction = fitnessFunction;
+    this.bestLocalPosition = _.clone(genome);
+    this.bestLocalFitness = fitnessFunction(genome);
+    this.fitness = fitnessFunction(genome);
+}
+
+Individual.prototype.walk = function(bestGlobalPosition, bestGlobalWeight){
+    this.genome = this.walkFunction(this.genome, this.bestLocalPosition, bestGlobalPosition, bestGlobalWeight); 
+    if(this.fitnessFunction(this.genome) > this.bestLocalFitness){
+        this.bestLocalFitness = this.fitnessFunction(this.genome);
+        this.bestLocalPosition = _.clone(this.genome);
+    }
+};
+
+module.exports = {
+    Individual: Individual
+};
---
+++ pso/population.js
@@ -0,0 +1,34 @@
+var _ = require('lodash');
+var individual = require('./individual.js');
+
+function Population( populationSize, populationGenerationFunction, walkFunction, fitnessFunction, globalPositionWeight){
+    this.individuals = _.map(populationGenerationFunction(populationSize), function(elem){ 
+        return new individual.Individual(elem, fitnessFunction, walkFunction);
+    });
+    
+    this.sortByFitness = function(){
+        this.individuals = _.sortBy(this.individuals, x => -x.fitness); // ordena do maior pro menor fitness
+    };
+    
+    this.bestSolution = null;
+
+    this.sortByFitness();
+    this.bestSolution = this.individuals[0];
+
+    this.iterate = function(displayBest){
+        _.each(this.individuals, function(elem){
+            elem.walk(this.bestSolution.genome, globalPositionWeight);
+        });      
+        this.sortByFitness();
+
+        // compara com melhor solucao anterior
+        if( (this.individuals[0]).fitness > (this.bestSolution != null ? this.bestSolution.fitness : -Math.min() ))
+            this.bestSolution = _.clone(this.individuals[0]);
+        if(displayBest)
+            console.log(this.bestSolution.fitness);
+    };
+}
+
+module.exports = {
+    Population: Population
+};
