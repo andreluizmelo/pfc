f03e2f5b3736c319212c77dee35107b10e0fd79a
Andre Luizm <andreluiz.melo@outlook.com>
2017-04-11 09:17:44 -0300

    problema menor soma de distancias

---
+++ problems/menorSomaDistancia.js
@@ -0,0 +1,62 @@
+var _ = require('lodash');
+
+
+
+function crossover(genome1, genome2){
+    return [
+        {
+            x: genome1.x,
+            y: genome2.y
+        },
+        {
+            x: genome2.x,
+            y: genome1.y
+        }
+    ];
+}
+
+function walk(currentPosition, bestLocalPosition, bestGlobalPosition, bestGlobalPositionWeight){
+    return {
+        x: currentPosition.x + ( bestLocalPosition.x - currentPosition.x) * (1 - bestGlobalPositionWeight) + ( bestGlobalPosition.x - currentPosition.x) * bestGlobalPositionWeight,
+        y: 0
+    }
+}
+
+function distancia(pointA, pointB){
+    return Math.sqrt(  (pointA.x - pointB.x) * (pointA.x - pointB.x) + (pointA.y - pointB.y) * (pointA.y - pointB.y));
+}
+
+function DistanciaProblem(width, height, pointList){ //dimensoes da area e lista de pontos nessa area    
+    this.crossoverFunction = crossover;
+    this.walkFunction = walk;
+    this.mutateFunction = function mutate(genome){
+        var newGenome = _.clone(genome);
+        if (Math.random() >= 0.5){
+            newGenome.x = Math.random() * width;
+        }else{
+            newGenome.y = Math.random() * height;
+        }
+        return newGenome;
+    };
+    this.fitnessFunction = function(genome){ // retorna 1 / (1 + soma das distancias ate os pontos)
+        return 1 / ( 1 + _.reduce(pointList, function(elem, res){ return res + distancia(elem, genome);}, 0));
+    };
+    function randomGenome(){
+        return {
+            x: Math.random() * width,
+            y: Math.random * height
+        };
+    }
+    this.generatePopulation = function(size){
+        var i;
+        population = [];
+        for(i = 0; i < size; i++){
+            population.push(randomGenome());
+        }
+        return population;
+    }
+}
+
+module.exports = {
+    DistanciaProblem: DistanciaProblem
+};
