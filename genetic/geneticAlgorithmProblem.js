1a7e15935bb00ade84a82aaeb06d654141bb84f7
Andre Luizm <andreluiz.melo@outlook.com>
2017-04-15 20:51:08 -0300

    colocando mais uma camada para simplicaficar passsagen de parametros e construcao da solucao

---
+++ genetic/geneticAlgorithmProblem.js
@@ -0,0 +1,59 @@
+var _ = require('lodash');
+var Population = require('./population.js');
+
+const defaultNumberOfSameResultToStop = 10;
+const maxNumberOfGeneratios = 10e6; 
+
+function CheckProblem(problem){
+    if( problem.generatePopulation == null || problem.generatePopulation == undefined)
+        throw new Error('função de geração de população nula');
+    if( problem.mutateFunction == null || problem.mutateFunction == undefined)
+        throw new Error('função de mutação nula');
+    if( problem.crossoverFunction == null || problem.crossoverFunction == undefined)
+        throw new Error('função de crossover nula');
+    if( problem.fitnessFunction == null || problem.fitnessFunction == undefined)
+        throw new Error('função de fitness nula');
+}
+
+function GeneticAlgorithmProblem(problem, populationSize, mutationProbability, crossoverProbability){
+    CheckProblem(problem);
+
+    this.population = Population.Population(populationSize,
+    problem.generatePopulation, problem.mutateFunction, problem.crossoverFunction,
+    problem.fitnessFunction, mutationProbability, crossoverProbability);
+}
+
+GeneticAlgorithmProblem.prototype.displayCurrentSolution = function(){
+    console.log('fitness: ' + this.population.bestSolution.fitness);
+    console.log('solução: ' + this.population.bestSolution.genome);
+};
+
+GeneticAlgorithmProblem.prototype.solveByNumberOfIterations = function(numberOfIterations, displayCurrentBest){
+    var i;
+    for(i = 0; i < numberOfIterations; i++){
+        this.population.iterate(displayCurrentBest);
+    }
+    this.displayCurrentSolution();
+};
+
+GeneticAlgorithmProblem.prototype.solve = function( numberOfSameResultToStop){
+    var minimumRepetition = numberOfSameResultToStop || defaultNumberOfSameResultToStop;
+    var generation = 0;
+    var numberOfRepetitions = 0;
+    var bestFitness = null;
+    while( generation < maxNumberOfGeneratios && numberOfRepetitions < 10){
+        generation++;
+        this.population.iterate(false);
+        if(this.population.bestFitness == bestFitness){
+            numberOfRepetitions++;
+        }else{
+            numberOfRepetitions = 0;
+        }
+    }
+    console.log("geração: " + generation);
+    this.displayCurrentSolution();
+};
+
+module.exports = {
+    GeneticAlgorithmProblem: GeneticAlgorithmProblem
+};
