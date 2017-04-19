25a6419ff29d9548b2ced617f47231c5be93b855
Andre Luizm <andreluiz.melo@outlook.com>
2017-04-18 23:29:38 -0300

    carregando templates de outras páginas

--- interface/scripts/events.js
+++ interface/scripts/events.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+    
+    // load templates
+    $("section#templates div").each(function(index, elem){
+        $(elem).load('./interface/templates/' + $(elem).data('path') + '.html');
+    });
+    
     $("#welcome-btn").click(function(event){
         $("#welcome").hide();
         $("#content").show();
