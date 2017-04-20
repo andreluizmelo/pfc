1fcf091b5b4ead6b58f64636897d1d5df61f01f6
Andre Luizm <andreluiz.melo@outlook.com>
2017-04-19 22:35:42 -0300

    mudando nome de templates, confirmando que nao precisa ser um html completo

--- interface/scripts/events.js
+++ interface/scripts/events.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     
     // load templates
     $("section#templates div").each(function(index, elem){
-        $(elem).load('./interface/templates/' + $(elem).data('path') + '.html');
+        $(elem).load('./interface/templates/' + $(elem).data('path') + '.handlebars');
     });
     
     $("#welcome-btn").click(function(event){
